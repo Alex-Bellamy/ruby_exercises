7530cb219b4b60ade44491c760cb8f8e8c556141
Alex Bellamy <alexlbellamy@gmail.com>
2020-05-20 09:01:54 +0200

    initial commit

---
+++ my_group.rb
@@ -0,0 +1,6 @@
+my_group = Array.new
+person_1 = {:name => "Peter", :gender =>"male", :age => 34}
+person_2 = {:name => "Sofie", :gender =>"female", :age => 41}
+person_3 = {:name => "Lewis", :gender =>"male", :age => 27}
+my_group = person_1[:name], + person_2[:name], +person_3[:name]
+my_group.class
