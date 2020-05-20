9342bfdb569e54f192199db4d76f88a6fda347d3
Alex Bellamy <alexlbellamy@gmail.com>
2020-05-20 13:28:04 +0200

    operators exercise complete

---
+++ operators.rb
@@ -0,0 +1,35 @@
+a = 44
+b = 16
+
+a + b
+a - b
+a * b
+a / b
+a % b
+a**b
+
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+a <=> b
+
+c = a + b
+c += a
+c -= a
+c *= a
+c /= a
+c %a= a
+c **= a
+
+2 + 2 && 7 > 3
+2 + 2 && 7 < 3
+
+false || false
+true || false
+false || true
+true || true
+
+!false
